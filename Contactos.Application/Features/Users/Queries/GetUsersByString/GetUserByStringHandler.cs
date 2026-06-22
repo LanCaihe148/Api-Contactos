c6e78bd9b3f19cc9dcc7d9cc15d54cb09b4902fb
Efrain <lancaihe148@gmail.com>
2026-06-21 17:40:29 -0700

    X

---
+++ Contactos.Application/Features/Users/Queries/GetUsersByString/GetUserByStringHandler.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Contactos.Application.Contracts.Persistance;
+using Contactos.Application.Features.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contactos.Application.Features.Users.Queries.GetUsersByString
+{
+    public class GetUserByStringHandler : IRequestHandler<GetUserByStringQuery, PaginatedResult<UserByidDto>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetUserByStringHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<PaginatedResult<UserByidDto>> Handle(GetUserByStringQuery request, CancellationToken cancellationToken)
+        {
+
+            var query = await _unitOfWork.UserRepository.GetPagedAsync(
+                request.PageIndex,
+                request.PageSize,
+                request.SearchTerm,
+                cancellationToken);
+
+            
+            var mappedItems = _mapper.Map<List<UserByidDto>>(query.Items);
+
+            return new PaginatedResult<UserByidDto>(
+                mappedItems,
+                query.TotalCount,
+                query.PageIndex,
+                query.PageSize);
+
+        }
+    }
+}
---
+++ Contactos.Application/Features/Users/Queries/GetUsersByString/GetUserByStringQuery.cs
@@ -0,0 +1,27 @@
+﻿using Contactos.Application.Features.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contactos.Application.Features.Users.Queries.GetUsersByString
+{
+    public class GetUserByStringQuery : IRequest<PaginatedResult<UserByidDto>>
+    {
+        public int PageIndex { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        public string? SearchTerm { get; set; }
+        public string? SortBy { get; set; }
+        public bool SortDescending { get; set; } = false;
+
+        public GetUserByStringQuery(int pageIndex, int pageSize, string? searchTerm)
+        {
+            PageIndex = pageIndex;
+            PageSize = pageSize;
+            SearchTerm = searchTerm;
+
+        }
+    }
+}
